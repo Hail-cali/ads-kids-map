fd661aaae17239922390d81c993cda1a3b2e818c
Hail Cali <79245242+Hail-cali@users.noreply.github.com>
2026-06-29 19:30:22 +0900

    [KM-001] add common config for api (#1)
    - add mongo config
    - add jackson config

---
+++ kids-map-service/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id("org.springframework.boot") version "3.3.5"
+    id("io.spring.dependency-management") version "1.1.6"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+}
+
+group = "com.ads.kidsmap"
+version "0.0.1"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+
+    //
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    // ko logging
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
+
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ kids-map-service/src/main/kotlin/com/ads/kidsmap/adsKids/AdsKidsMapApplication.kt
@@ -0,0 +1,11 @@
+package com.ads.kidsmap.adsKids
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdsKidsMapApplication
+
+fun main(args: Array<String>) {
+    runApplication<AdsKidsMapApplication>(*args)
+}
