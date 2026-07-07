a69bdee5ff716f9500941989aab2389fc246a10f
Hail Cali <79245242+Hail-cali@users.noreply.github.com>
2026-07-07 23:02:24 +0900

    Set up local Docker tunnel entrypoint

--- kids-map-service/src/main/kotlin/com/ads/kidsmap/adsKids/common/config/KafkaProducerConfig.kt
+++ kids-map-service/src/main/kotlin/com/ads/kidsmap/adsKids/common/config/KafkaProducerConfig.kt
@@ -3,6 +3,7 @@ package com.ads.kidsmap.adsKids.common.config
 import com.ads.kidsmap.adsKids.adstracker.payload.TrackingEvent
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
@@ -13,9 +14,11 @@ import org.springframework.kafka.support.serializer.JsonSerializer
 class KafkaProducerConfig {
 
     @Bean
-    fun kafkaTemplate(): KafkaTemplate<String, TrackingEvent> {
+    fun kafkaTemplate(
+        @Value("\${spring.kafka.bootstrap-servers}") bootstrapServers: String,
+    ): KafkaTemplate<String, TrackingEvent> {
         val props = mapOf(
-            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
             ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
             ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to JsonSerializer::class.java,
             ProducerConfig.ACKS_CONFIG to "all",
